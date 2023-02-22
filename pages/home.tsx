003d18a2a4c4c37949bce0fc906605e0c7e06a3d
Angel Scutari <antonioscutari2@hotmail.com>
2023-02-21 23:35:13 -0300

    commenting layout

---
+++ pages/home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home(){
+    //Home contains everything
+    //NavBar it's going to contain the Session functions
+  return (
+    <>
+    {/* <NavBar></NavBar> */}
+    <div>
+        {/* <Carrousel></Carrousel> */}
+        {/*<Articles/> */}
+        {/*<Latest Readings/>*/}
+        {/*<Calendar/> */}
+        {/* <WallOfLove/> */}
+        <footer/>
+    </div>
+    </>
+  )
+}
+
+export default Home
