eb55377848a6626a24b614569ba5a66d0954c236
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-28 13:16:17 -0300

    [ADD] Landing

---
+++ pages/Landing.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+
+const logoCristal = "/public/logocristal.png";
+
+function Landing(): React.ReactElement {
+  return (
+    <>
+      <Image src={logoCristal} height={550} width={447} alt="logo" priority />
+      <header className={styles.header}>
+        <h1>Cristal de Luna</h1>
+        <p className={styles.subheader}>TAROT</p>
+      </header>
+      <button className={styles.landingButton}>Entrar</button>
+      <button className={styles.landingButton}>Registrarse</button>
+    </>
+  );
+}
+
+export default Landing;
