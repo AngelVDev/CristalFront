185ff34974cfa4147ace71ac670d2a9e12041795
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-30 01:42:31 -0300

    [FIX] imports

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import styles from "@/styles/Home.module.css";
-import Image from "next/image";
+import styles from "../styles/Home.module.css";
+// import Image from "next/image";
 
 const logoCristal = "/public/logocristal.png";
 
 function Landing(): React.ReactElement {
   return (
     <>
-      <Image src={logoCristal} height={550} width={447} alt="logo" priority />
+      <img src={logoCristal} height={550} width={447} alt="logo" />
       <header className={styles.header}>
         <h1>Cristal de Luna</h1>
         <p className={styles.subheader}>TAROT</p>
