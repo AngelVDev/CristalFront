a570ede5984d94eb72a02acc30a774100619207c
Angel Scutari <antonioscutari2@hotmail.com>
2023-05-02 23:56:20 -0300

    [MOD] Form comentado

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
 import Landing from "./Landing";
-import Form from "./Form";
+// import Form from "./Form";
 
 export default function Home(): React.ReactNode {
   return (
@@ -15,7 +15,7 @@ export default function Home(): React.ReactNode {
       </Head>
       <main className={styles.main}>
         <Landing />
-        <Form />
+        {/* <Form /> */}
       </main>
       <footer />
     </>
