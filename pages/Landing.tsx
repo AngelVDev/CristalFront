d8c7549d5146f592f8040afdae04ad253a7e9b24
Angel Scutari <antonioscutari2@hotmail.com>
2023-05-02 23:56:46 -0300

    [ADD] Modal

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
-// import Image from "next/image";
+import Image from "next/image";
 
-const logoCristal = "/public/logocristal.png";
+import logoCristal from "/public/logocristal.png";
+
+import Login from "../components/Login";
 
 function Landing(): React.ReactElement {
   return (
     <>
-      <img src={logoCristal} height={550} width={447} alt="logo" />
+      <Image
+        className={styles.logoLanding}
+        src={logoCristal}
+        height={220}
+        width={160}
+        alt="logo"
+        priority
+      />
       <header className={styles.header}>
         <h1>Cristal de Luna</h1>
         <p className={styles.subheader}>TAROT</p>
       </header>
-      <button className={styles.landingButton}>Entrar</button>
+      <button
+        className={styles.landingButton}
+        onClick={() => document.querySelector("#modalDialog")?.showModal()}
+      >
+        Entrar
+      </button>
+      <dialog id="modalDialog" className={styles.formContainer}>
+        <Login />
+      </dialog>
       <button className={styles.landingButton}>Registrarse</button>
     </>
   );
