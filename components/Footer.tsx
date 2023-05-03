17f63842444c1c40ce21afbf821e377a52ec7b41
Angel Scutari <antonioscutari2@hotmail.com>
2023-05-03 17:24:23 -0300

    [NEW] Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <section>
+        <p>Instagram</p>
+        <p>TikTok</p>
+        <p>Facebook</p>
+      </section>
+      <section>
+        <p>Cookies</p>
+        <p>Privacidad</p>
+      </section>
+      Copyright 2023 Cristal De Luna Tarot
+    </footer>
+  );
+};
+
+/* 
+Copyright 2023 Cristal De Luna Tarot
+Redes
+Cookies
+Privacidad
+*/
+
+export default Footer;
