4bedf9b1ed0eba666148fabe96b00125963e5d6d
Angel Scutari <antonioscutari2@hotmail.com>
2023-10-28 23:29:43 -0300

    feat: home page = On

---
+++ pages/home.tsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <main>
+        <Navbar />
+        This is like home
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
