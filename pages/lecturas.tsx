912b66ed0e4e00186e58ee8ae5573310dfc449f1
Angel Scutari <antonioscutari2@hotmail.com>
2023-10-30 03:40:14 -0300

    feature: "lecturas" page is on

--- pages/lecturas.tsx
+++ pages/lecturas.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/lecturas.module.css";
 import Card from "../components/Card/Card";
 import { Service } from "../models/service.model";
 import { Services } from "../models/services";
+import RootLayout from "./layout";
 
 type Props = Service[];
 const services: Props = Services.filter(
@@ -10,15 +11,17 @@ const services: Props = Services.filter(
 
 const Lecturas = () => {
   return (
-    <main>
-      <header className={styles.lecturas_header}>Todas mis lecturas</header>
-      <section className={styles.cards_container}>
-        {services.map((srv) => (
-          // staticData && staticData.map(srv => (
-          <Card key={srv.name} props={srv} />
-        ))}
-      </section>
-    </main>
+    <RootLayout>
+      <main>
+        <header className={styles.lecturas_header}>Todas mis lecturas</header>
+        <section className={styles.cards_container}>
+          {services.map((srv) => (
+            // staticData && staticData.map(srv => (
+            <Card key={srv.name} props={srv} />
+          ))}
+        </section>
+      </main>
+    </RootLayout>
   );
 };
 
