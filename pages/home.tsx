b7b0b595174782bf9fdf8259e209e3b26214d639
Angel Scutari <antonioscutari2@hotmail.com>
2023-10-30 03:52:00 -0300

    refactor: home is now wrapped in layout

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,15 @@
-import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/Navbar";
+// import Footer from "../components/Footer/Footer";
+// import Navbar from "../components/Navbar/Navbar";
+
+import RootLayout from "./layout";
 
 const Home = () => {
-  return (
-    <>
-      <main>
-        <Navbar />
-        This is like home
-      </main>
-      <Footer />
-    </>
-  );
+  // const staticData = fetch(
+  //   "https://cristal-de-luna-api.vercel.app/srv/servicios",
+  //   { cache: "force-cache" }
+  // );
+
+  return <RootLayout>holo</RootLayout>;
 };
 
 export default Home;
